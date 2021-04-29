8e534a96d9c94ed841c6ce7d2d8709301158654e
hp <hp@163.com>
2021-04-29 20:02:56 +0800

    first commit

---
+++ src/Test.java
@@ -0,0 +1,11 @@
+/**
+ * @author shkstart
+ * @create 2021-04-29 19:58
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("1111");
+        System.out.println("1111");
+    }
+}
